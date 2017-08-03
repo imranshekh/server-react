8a348ad499ea9bc5f156869d7464a1c2967b9bf0
Anjna <anjnaharsora@gmail.com>
2017-08-03 12:46:51 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lcom64_one on 8/2/2017.
+ */
+module.exports={
+  googleClientID:'786158835564-r4s5ckn3auhhjmtaas9ncns9ebno23ne.apps.googleusercontent.com',
+  googleClientSecret:'49wDd-FvyJEHA-e3WIZ0Zuz0'
+};
--- index.js
+++ index.js
@@ -2,15 +2,40 @@
  * Created by lcom64_one on 8/2/2017.
  */
 const express=require('express');
-
+const passport=require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys=require('./config/keys');
 const app=express();
 
-//Route handler for getting the data
+//pa ssport for handling all backend congiguration google oauth api
 
-app.get('/',(req,res)=>{
+passport.use(
+  new GoogleStrategy(
+     {
+       clientID: keys.googleClientID,
+       clientSecret: keys.googleClientSecret,
+       callbackURL: '/auth/google/callback'
+     },
+    (accessToken,refreshToken,profile,done) => {
+       console.log('accesToken:',accessToken );
+       console.log('refreshToken:',refreshToken);
+       console.log('profile:',profile);
+     }
+  )
+);
 
-  res.send({hi:'imran'});
+//Route handler to get the google api Code
 
-});
+app.get(
+  '/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+  })
+);
 
-app.listen(5001);
+//Route handler to pass Code to google for configuration
+
+app.get('/auth/google/callback',passport.authenticate('google'));
+
+
+const PORT=process.env.port || 5001;
+app.listen(PORT);
